f80e845c7adf7a1787870b60bcbcc10760d67517
Marcio Brito <marcio.brito@outlook.com>
2020-06-17 10:32:16 -0300

    Criacao das entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string  Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+    }
+}
