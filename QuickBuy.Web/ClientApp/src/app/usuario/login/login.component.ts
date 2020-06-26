a4cfe2e4fa6471ad166090378fc4504241a4a5c3
Marcio Brito <marcio.brito@outlook.com>
2020-06-26 15:19:45 -0300

    melhoria nas mensagens de validação, ngClass e boostrap

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+
+
+export class LoginComponent {
+  public usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  public copyright = "Desenvolvido por: Marcio Brito";
+  public email = "";
+  public senha = "";
+
+  acessar() {
+    
+  }
+}
